0ae0fd00994a8c5f6260be72ce3a2c67c8ca6a04
Henrik Stromberg <henrik@askdrq.com>
2024-01-02 22:50:09 +0100

    cast added

--- src/test_super_matrix.rs
+++ src/test_super_matrix.rs
@@ -1,5 +1,7 @@
 #[cfg(test)]
 mod tests {
+    use std::mem::MaybeUninit;
+    use std::slice::from_raw_parts_mut;
     use crate::super_matrix::SuperMatrix;
     use sprs::{CsMat, TriMat};
     use ndarray::Array2;
@@ -107,5 +109,71 @@ mod tests {
             }
             assert!(!super_matrix.raw().Store.is_null());
         }
+    }
+
+    #[test]
+    fn test_from_raw() {
+        let mat = unsafe {
+            let (m, n, nnz) = (5, 5, 12);
+
+            let a = ffi::doubleMalloc(nnz);
+            assert!(!a.is_null());
+            {
+                let (s, u, p, e, r, l) = (19.0, 21.0, 16.0, 5.0, 18.0, 12.0);
+                let a = from_raw_parts_mut(a, nnz as usize);
+                a[0] = s;
+                a[1] = l;
+                a[2] = l;
+                a[3] = u;
+                a[4] = l;
+                a[5] = l;
+                a[6] = u;
+                a[7] = p;
+                a[8] = u;
+                a[9] = e;
+                a[10] = u;
+                a[11] = r;
+            }
+
+            let asub = ffi::intMalloc(nnz);
+            assert!(!asub.is_null());
+            {
+                let asub = from_raw_parts_mut(asub, nnz as usize);
+                asub[0] = 0;
+                asub[1] = 1;
+                asub[2] = 4;
+                asub[3] = 1;
+                asub[4] = 2;
+                asub[5] = 4;
+                asub[6] = 0;
+                asub[7] = 2;
+                asub[8] = 0;
+                asub[9] = 3;
+                asub[10] = 3;
+                asub[11] = 4;
+            }
+
+            let xa = ffi::intMalloc(n + 1);
+            assert!(!xa.is_null());
+            {
+                let xa = from_raw_parts_mut(xa, (n + 1) as usize);
+                xa[0] = 0;
+                xa[1] = 3;
+                xa[2] = 6;
+                xa[3] = 8;
+                xa[4] = 10;
+                xa[5] = 12;
+            }
+
+            let mut mat: ffi::SuperMatrix = MaybeUninit::zeroed().assume_init();
+
+            ffi::dCreate_CompCol_Matrix(&mut mat, m, n, nnz, a, asub, xa, Stype_t::SLU_NC, Dtype_t::SLU_D, Mtype_t::SLU_GE);
+            mat
+        };
+        let super_mat = unsafe {SuperMatrix::from_raw(mat)};
+        let csc_opt = super_mat.into_csc_mat();
+        let csc = csc_opt.unwrap();
+        println!("{:?}", csc)
+        // ToDo: Debug cast
     }
 }
