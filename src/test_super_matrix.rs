228531e40e2cf57af85308c7d2c6f1c70f9783cc
Henrik Stromberg <henrik@askdrq.com>
2024-01-03 19:59:11 +0100

    refactoring

--- src/test_super_matrix.rs
+++ src/test_super_matrix.rs
@@ -1,7 +1,6 @@
 #[cfg(test)]
 mod tests {
     use std::mem::MaybeUninit;
-    use std::slice::from_raw_parts_mut;
     use crate::super_matrix::SuperMatrix;
     use sprs::{CsMat, TriMat};
     use ndarray::{arr1, arr2, Array1, Array2};
@@ -36,9 +35,6 @@ mod tests {
 
     #[test]
     fn test_from_csc_mat_basic() {
-        use ffi::Dtype_t::*;
-        use ffi::Mtype_t::*;
-        use ffi::Stype_t::*;
         use ffi::colperm_t::*;
 
         let values = vec![19.0, 12.0, 12.0, 21.0, 12.0, 12.0, 21.0, 16.0, 21.0, 5.0, 21.0, 18.0];
@@ -50,7 +46,7 @@ mod tests {
         let mut b_mat = SuperMatrix::from_ndarray(arr2(&[[1., 1., 1., 1., 1.]]).t().to_owned());
         let res = unsafe {
 
-            let (m, n, nnz) = (5, 5, 12);
+            let (m, n) = (5, 5);
 
             let perm_r = ffi::intMalloc(m);
             assert!(!perm_r.is_null());
@@ -204,21 +200,19 @@ mod tests {
 
         let super_matrix = SuperMatrix::from_ndarray(array.clone());
 
-        unsafe {
-            assert_eq!(super_matrix.nrows(), 2);
-            assert_eq!(super_matrix.ncols(), 2);
-            match super_matrix.raw().Stype {
-                Stype_t::SLU_DN => {}
-                _ => panic!("Stype != SLU_DN"),
-            }
-            match super_matrix.raw().Dtype {
-                Dtype_t::SLU_D => {}
-                _ => panic!("Dtype != SLU_D"),
-            }
-            match super_matrix.raw().Mtype {
-                Mtype_t::SLU_GE => {}
-                _ => panic!("Mtype != SLU_GE"),
-            }
+        assert_eq!(super_matrix.nrows(), 2);
+        assert_eq!(super_matrix.ncols(), 2);
+        match super_matrix.raw().Stype {
+            Stype_t::SLU_DN => {}
+            _ => panic!("Stype != SLU_DN"),
+        }
+        match super_matrix.raw().Dtype {
+            Dtype_t::SLU_D => {}
+            _ => panic!("Dtype != SLU_D"),
+        }
+        match super_matrix.raw().Mtype {
+            Mtype_t::SLU_GE => {}
+            _ => panic!("Mtype != SLU_GE"),
         }
         let back_conversion = super_matrix.into_ndarray().unwrap();
         assert!(array2s_close(&array, &back_conversion, 0.01));
@@ -231,84 +225,20 @@ mod tests {
 
         let super_matrix = SuperMatrix::from_ndarray(array);
 
-        unsafe {
-            assert_eq!(super_matrix.nrows(), 2);
-            assert_eq!(super_matrix.ncols(), 2);
-            match super_matrix.raw().Stype {
-                Stype_t::SLU_DN => {}
-                _ => {panic!("Stype!=SLU_DN")}
-            }
-            match super_matrix.raw().Dtype {
-                Dtype_t::SLU_D => {}
-                _ => {panic!("Dtype!=SLU_D")}
-            }
-            match super_matrix.raw().Mtype {
-                Mtype_t::SLU_GE => {}
-                _ => {panic!("Mtype!=SLU_GE")}
-            }
-            assert!(!super_matrix.raw().Store.is_null());
+        assert_eq!(super_matrix.nrows(), 2);
+        assert_eq!(super_matrix.ncols(), 2);
+        match super_matrix.raw().Stype {
+            Stype_t::SLU_DN => {}
+            _ => {panic!("Stype!=SLU_DN")}
         }
-    }
-
-    #[test]
-    fn test_from_raw() {
-        let mat = unsafe {
-            let (m, n, nnz) = (5, 5, 12);
-
-            let a = ffi::doubleMalloc(nnz);
-            assert!(!a.is_null());
-            {
-                let (s, u, p, e, r, l) = (19.0, 21.0, 16.0, 5.0, 18.0, 12.0);
-                let a = from_raw_parts_mut(a, nnz as usize);
-                a[0] = s;
-                a[1] = l;
-                a[2] = l;
-                a[3] = u;
-                a[4] = l;
-                a[5] = l;
-                a[6] = u;
-                a[7] = p;
-                a[8] = u;
-                a[9] = e;
-                a[10] = u;
-                a[11] = r;
-            }
-
-            let asub = ffi::intMalloc(nnz);
-            assert!(!asub.is_null());
-            {
-                let asub = from_raw_parts_mut(asub, nnz as usize);
-                asub[0] = 0;
-                asub[1] = 1;
-                asub[2] = 4;
-                asub[3] = 1;
-                asub[4] = 2;
-                asub[5] = 4;
-                asub[6] = 0;
-                asub[7] = 2;
-                asub[8] = 0;
-                asub[9] = 3;
-                asub[10] = 3;
-                asub[11] = 4;
-            }
-
-            let xa = ffi::intMalloc(n + 1);
-            assert!(!xa.is_null());
-            {
-                let xa = from_raw_parts_mut(xa, (n + 1) as usize);
-                xa[0] = 0;
-                xa[1] = 3;
-                xa[2] = 6;
-                xa[3] = 8;
-                xa[4] = 10;
-                xa[5] = 12;
-            }
-
-            let mut mat: ffi::SuperMatrix = MaybeUninit::zeroed().assume_init();
-
-            ffi::dCreate_CompCol_Matrix(&mut mat, m, n, nnz, a, asub, xa, Stype_t::SLU_NC, Dtype_t::SLU_D, Mtype_t::SLU_GE);
-            mat
-        };
-        let super_mat = unsafe {SuperMatrix::from_raw(mat)};
+        match super_matrix.raw().Dtype {
+            Dtype_t::SLU_D => {}
+            _ => {panic!("Dtype!=SLU_D")}
+        }
+        match super_matrix.raw().Mtype {
+            Mtype_t::SLU_GE => {}
+            _ => {panic!("Mtype!=SLU_GE")}
+        }
+        assert!(!super_matrix.raw().Store.is_null());
     }
 }
