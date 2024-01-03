28ec9585be1fe2856dc32e91e73d3ee2c3e9eb10
Henrik Stromberg <henrik@askdrq.com>
2024-01-03 20:07:48 +0100

    Option default generator improved

--- src/solver.rs
+++ src/solver.rs
@@ -22,38 +22,10 @@ pub struct Options {
 
 impl Default for Options {
     fn default() -> Self {
+        let mut options: superlu_options_t = unsafe {MaybeUninit::zeroed().assume_init()};
+        unsafe { ffi::set_default_options(&mut options); }
         Self {
-            ffi:
-            superlu_options_t {
-                Fact: superlu_sys::fact_t::DOFACT,
-                Equil: yes_no_t::YES,
-                ColPerm: superlu_sys::colperm_t::COLAMD,
-                Trans: superlu_sys::trans_t::NOTRANS,
-                IterRefine: superlu_sys::IterRefine_t::NOREFINE,
-                DiagPivotThresh: 1.0,
-                SymmetricMode: yes_no_t::NO,
-                PivotGrowth: yes_no_t::NO,
-                ConditionNumber: yes_no_t::NO,
-                PrintStat: yes_no_t::YES,
-                RowPerm: superlu_sys::rowperm_t::LargeDiag,
-                ILU_DropRule: 0,
-                ILU_DropTol: 0.0,
-                ILU_FillFactor: 0.0,
-                ILU_Norm: superlu_sys::norm_t::ONE_NORM,
-                ILU_FillTol: 0.0,
-                ILU_MILU: superlu_sys::milu_t::SILU,
-                ILU_MILU_Dim: 0.0,
-                //The following fields are probably unused
-                ParSymbFact: yes_no_t::NO,
-                ReplaceTinyPivot: yes_no_t::NO,
-                SolveInitialized: yes_no_t::NO,
-                RefineInitialized: yes_no_t::NO,
-                nnzL: 0,
-                nnzU: 0,
-                num_lookaheads: 0,
-                lookahead_etree: yes_no_t::NO,
-                SymPattern: yes_no_t::NO,
-            }
+            ffi: options
         }
     }
 }
